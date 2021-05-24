c091b9a8a132dfe6ef4cb016bb8800ec5070dfd3
joschiservice <joschua.hass.sh@gmail.com>
2021-05-24 12:55:18 +0200

    worked on navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,14 +1,23 @@
-import styled from 'styled-components';
-import {Container} from '@material-ui/core';
+import styled from "styled-components";
+import { Container } from "@material-ui/core";
 
 export default function Navbar() {
-  return <NavbarWrapper>
-
-  </NavbarWrapper>;
+  return (
+  <NavbarWrapper>
+    <Container>
+      <img src="/horizon.svg"/>
+      <Title>Horizon</Title>
+    </Container>
+  </NavbarWrapper>);
 }
 
 const NavbarWrapper = styled.div`
-background-color: #424242;
-height: 50px;
-width: 100%;
+  background-color: #424242;
+  height: 50px;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+margin: 0;
+font-size: large;
 `;
