a6747043d687c171efeb32e58280dff0403021f2
Joschua H <54814804+joschiservice@users.noreply.github.com>
2021-09-17 10:08:24 +0200

    added auth stuff to the sidebar

--- components/navigation/SideBar/SideBarItem.tsx
+++ components/navigation/SideBar/SideBarItem.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link';
+
 interface Props {
   icon: React.ReactNode;
+  title: string;
+  href: string;
 }
-  
+
 export function SideBarItem(props: Props) {
   return (
-      <div className="sidebar-item text-blue-200 text-2xl p-6 flex items-center transition-all duration-100 hover:bg-opacity-20 bg-black bg-opacity-0">
+    <Link href={props.href} passHref>
+      <a title={props.title}
+        className="sidebar-item text-blue-200 text-2xl p-6 flex items-center transition-all duration-100 hover:bg-opacity-20 bg-black bg-opacity-0">
         {props.icon}
-      </div>
+      </a>
+    </Link>
   );
 }
--- components/navigation/SideBar/index.tsx
+++ components/navigation/SideBar/index.tsx
@@ -1,17 +1,30 @@
 import { SideBarItem } from './SideBarItem';
 
-import { IoHomeOutline } from 'react-icons/io5';
+import { IoHomeOutline, IoLogInOutline, IoLogOutOutline } from 'react-icons/io5';
 import { AiOutlineAppstore } from 'react-icons/ai';
+import { useAuth } from '../../../contexts/AuthContext';
 
 interface Props {
-  children?: React.ReactNode;
+  TopItems?: React.ReactNode;
+  BottomItems?: React.ReactNode;
 }
   
 export function SideBar(props: Props) {
+  const auth = useAuth();
+
   return (
-      <div className="border-r border-white border-opacity-25">
-        <SideBarItem icon={ <IoHomeOutline /> } />
-        <SideBarItem icon={ <AiOutlineAppstore /> } />
+      <div className="border-r border-white border-opacity-25 flex-col flex justify-between">
+        <div>
+          <SideBarItem icon={ <IoHomeOutline /> } title="Home" href="/" />
+          <SideBarItem icon={ <AiOutlineAppstore /> } title="Library" href="/" />
+        </div>
+        { auth.isAuthenticated ? 
+        <div>
+          <SideBarItem icon={ <IoLogOutOutline /> } title="Sign Out" href="/auth/logout" />
+        </div> : 
+        <div>
+        <SideBarItem icon={ <IoLogInOutline /> } title="Sign In" href="/auth/login" />
+        </div> }
       </div>
   );
 }
