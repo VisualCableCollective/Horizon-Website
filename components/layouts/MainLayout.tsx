f67d39abd7b60f91e42824df88e9085dcee09f3d
Joschua H <54814804+joschiservice@users.noreply.github.com>
2021-09-17 14:15:48 +0200

    optimized layout

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -8,8 +8,10 @@ export function MainLayout(props: Props) {
   return (
       <div className="bg-gradient-to-tr from-purple-900 to-blue-600 min-h-screen flex text-white">
         <SideBar />
-        <div className="w-full">
-          {props.children}
+        <div className="w-full flex justify-center">
+          <div className="m-6 max-w-screen-2xl w-full">
+            {props.children}
+          </div>
         </div>
       </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,22 +23,20 @@ export default function Home() {
 
   return (
     <MainLayout>
-      <div className="m-10 max-w-screen-2xl mx-auto">
-        <div className="news-wrapper h-96 rounded bg-center bg-cover w-full" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1616588589676-62b3bd4ff6d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80")' }}>
-          <div className="flex justify-center items-center bg-black bg-opacity-40 h-full rounded">
-            <div className="max-w-screen-md shadow">
-              <h1 className="text-4xl font-bold text-center mb-2">Welcome to the Horizon App Store!</h1>
-              <p className="text-center">The Horizon storefront is currently under heavy development. You can find the latest information about our development progress on our website and on our Discord server.</p>
-            </div>
+      <div className="news-wrapper h-96 rounded bg-center bg-cover w-full" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1616588589676-62b3bd4ff6d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80")' }}>
+        <div className="flex justify-center items-center bg-black bg-opacity-40 h-full rounded">
+          <div className="max-w-screen-md shadow">
+            <h1 className="text-4xl font-bold text-center mb-2">Welcome to the Horizon App Store!</h1>
+            <p className="text-center">The Horizon storefront is currently under heavy development. You can find the latest information about our development progress on our website and on our Discord server.</p>
           </div>
         </div>
-        <h1 className="text-xl font-bold mb-4 mt-6">Products by The VisualCable Collective</h1>
-        <div className="products-row flex flex-wrap gap-4">
-          { vccTeamData && vccTeamData.products.map((product) => {
-            product.creator = vccTeamData;
-            return <ProductPreviewItem key={product.id} style={PreviewStyle.Store} product={product} />;
-          }) }
-        </div>
+      </div>
+      <h1 className="text-xl font-bold mb-4 mt-6">Products by The VisualCable Collective</h1>
+      <div className="products-row flex flex-wrap gap-4">
+        {vccTeamData && vccTeamData.products.map((product) => {
+          product.creator = vccTeamData;
+          return <ProductPreviewItem key={product.id} style={PreviewStyle.Store} product={product} />;
+        })}
       </div>
     </MainLayout>
   );
